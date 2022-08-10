0c93e5065fc5c0e04267b95ed4aa7302e703814d
Mohammed Ragab Saad <103288444+MohammedRSaad@users.noreply.github.com>
2022-08-10 21:03:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import datetime as dt
 import pandas as pd
 import tkinter as tk
+from tkinter import ttk
 
 class students:
     def __init__(self, Id, name):
@@ -15,17 +16,17 @@ class classrooms:
         student = students(Id, name)
         self.students.loc[self.students.shape[0]+1] = student.info
     
-    def remove_student(self, student):
-        self.students.drop(self.students.index[self.students["id"] == student[0]], inplace = True)
+    def remove_student(self, name):
+        self.students.drop(self.students.index[self.students["name"] == name], inplace = True)      
     
-    def take_attendance(self, name=None):
+    def take_attendence(self, name=None, Id=None):
         date = dt.datetime.now().strftime("%Y-%m-%d") #if you want to test add: %H:%M:%S
         last = str(self.students.columns.values[-1])
         
         if last == "name" or date > last:
             self.students[f"{date}"] = [1] * len(self.students)
         
-        self.students[f"{date}"][self.students['name'] == name] = 0
+        self.students[f"{date}"][self.students['id'] == Id] = 0
     
     def get_attendence(self):
         return self.students.iloc[:, 2:].sum(axis=1)
@@ -34,10 +35,24 @@ class schools:
     def __init__(self):
         self.classes = []
         self.names = []
+        
     def add_classroom(self, name):
         self.names.append(name)
         a = classrooms(name)
         self.classes.append(a)
+        
+    def transfer_student(self, name, to_class, from_class):
+        to_class.students.loc[self.students.shape[0]+1] = to_class.students[self.students["name"] == name]
+        to_class.remove_student(name)
+    
+    def get_class(self, name):
+        idx = 0
+        if self.names and name in self.names:
+            idx = self.names.index(name)
+        else: return [" "]
+            
+        return list(self.classes[idx].students['name'])
+    
     def show_classrooms(self):
         for room in self.classes:
             print (room.students)
@@ -50,102 +65,131 @@ class App(tk.Tk):
         self.geometry('500x300')
         
     def main_win(self, school):
-        self.main = tk.Frame(self)
+        self.main = ttk.Frame(self)
         self.main.pack(fill="both", expand = True)
         
-        new_classroom = tk.Button(self.main, text='New Classroom', command=lambda:\
+        new_classroom = ttk.Button(self.main, text='New Classroom', command=lambda:\
                       [self.new_classroom_win(school), self.main.pack_forget()])
         new_classroom.grid(column=0, row=0, sticky=tk.E, padx=5, pady=5)
         
-        new_student = tk.Button(self.main, text='Add Student', command=lambda:\
+        new_student = ttk.Button(self.main, text='Add Student', command=lambda:\
                       [self.add_student_win(school), self.main.pack_forget()])
         new_student.grid(column=0, row=1, sticky=tk.E, padx=5, pady=5)
-
-        show = tk.Button(self.main, text='show Classrooms', command=lambda:\
-                      [school.show_classrooms()])
-        show.grid(column=0, row=2, sticky=tk.E, padx=5, pady=5)
-
-        take_attendance = tk.Button(self.main, text='Take Attendance', command=lambda:\
+        
+        transefer_frame = ttk.Button(self.main, text='Transfer Student', command=lambda:\
+                      [self.transfer_win(school), self.main.pack_forget()])
+        transefer_frame.grid(column=0, row=2, sticky=tk.E, padx=5, pady=5)
+        
+        
+        take_attendance = ttk.Button(self.main, text='Take Attendance', command=lambda:\
                       [self.take_attendance_win(school), self.main.pack_forget()])
         take_attendance.grid(column=0, row=3, sticky=tk.E, padx=5, pady=5)
+        
+        show = ttk.Button(self.main, text='show Classrooms', command=lambda:\
+                      [school.show_classrooms()])
+        show.grid(column=0, row=4, sticky=tk.E, padx=5, pady=5)
     
     def new_classroom_win(self, school):
-        self.new_classroom = tk.Frame(self)
+        self.new_classroom = ttk.Frame(self)
         self.new_classroom.pack(fill="both", expand = True)
 
-        label = tk.Label(self.new_classroom, text="Class Name: ")
+        label = ttk.Label(self.new_classroom, text="Class Name: ")
         label.grid(column=0, row=0, sticky=tk.W, padx=5, pady=5)
-        name = tk.Entry(self.new_classroom)
+        name = ttk.Entry(self.new_classroom)
         name.grid(column=1, row=0, sticky=tk.E, padx=5, pady=5)
         
-        submit = tk.Button(self.new_classroom, text='Submit', command=lambda:\
+        submit = ttk.Button(self.new_classroom, text='submit', command=lambda:\
                       [school.add_classroom(name.get())])
         submit.grid(column=2, row=3, sticky=tk.E, padx=5, pady=5)
         
-        main_menu_button = tk.Button(self.new_classroom, text='Main Menu', command=lambda:\
+        main_menu_button = ttk.Button(self.new_classroom, text='Main Menu', command=lambda:\
                       [self.main_win(school), self.new_classroom.pack_forget()])
         main_menu_button.grid(column=1, row=3, sticky=tk.E, padx=5, pady=5)
         
     def add_student_win(self, school):
-        self.new_student = tk.Frame(self)
+        self.new_student = ttk.Frame(self)
         self.new_student.pack(fill="both", expand = True)
 
-        label = tk.Label(self.new_student, text="Class Name: ")
+        label = ttk.Label(self.new_student, text="Class Name: ")
         label.grid(column=0, row=0, sticky=tk.W, padx=5, pady=5)
-        name_var = tk.StringVar()
-        name_var.set("Choose")
-        name = tk.OptionMenu(self.new_student, name_var, *school.names if school.names else ["No classes"])
-        name.grid(column=1, row=0, sticky=tk.W, padx=5, pady=5)
+        name_list = ttk.Combobox(self.new_student, value=school.names)
+        name_list.grid(column=1, row=0, sticky=tk.W, padx=5, pady=5)
         
-        
-        label = tk.Label(self.new_student, text="ID: ")
+        label = ttk.Label(self.new_student, text="ID: ")
         label.grid(column=0, row=2, sticky=tk.W, padx=5, pady=5)
-        Id = tk.Entry(self.new_student)
+        Id = ttk.Entry(self.new_student)
         Id.grid(column=1, row=2, sticky=tk.E, padx=5, pady=5)
         
-        label = tk.Label(self.new_student, text="Name: ")
+        label = ttk.Label(self.new_student, text="Name: ")
         label.grid(column=0, row=3, sticky=tk.W, padx=5, pady=5)
-        name = tk.Entry(self.new_student)
+        name = ttk.Entry(self.new_student)
         name.grid(column=1, row=3, sticky=tk.E, padx=5, pady=5)
         
-        submit = tk.Button(self.new_student, text='Submit', command=lambda:\
-                      [school.classes[school.names.index(name_var.get())].add_student(Id.get(), name.get())])
-        submit.grid(column=2, row=5, sticky=tk.E, padx=5, pady=5)
+        submit = ttk.Button(self.new_student, text='submit', command=lambda:\
+                      [school.classes[school.names.index(name_list.get())].add_student(Id.get(), name.get())])
+        submit.grid(column=2, row=4, sticky=tk.E, padx=5, pady=5)
         
-        main_menu_button = tk.Button(self.new_student, text='Main Menu', command=lambda:\
+        main_menu_button = ttk.Button(self.new_student, text='Main Menu', command=lambda:\
                       [self.main_win(school), self.new_student.pack_forget()])
-        main_menu_button.grid(column=1, row=5, sticky=tk.E, padx=5, pady=5)
-
+        main_menu_button.grid(column=1, row=4, sticky=tk.E, padx=5, pady=5)
+    
+    def transfer_win(self, school):
+        self.transfer_frame = ttk.Frame(self)
+        self.transfer_frame.pack(fill="both", expand = True)
+        
+        def get_students_list(e):
+            student_name.config(value=school.get_class(from_class.get()))
+        
+        label = ttk.Label(self.transfer_frame, text="From Class: ")
+        label.grid(column=0, row=0, sticky=tk.W, padx=5, pady=5)
+        from_class = ttk.Combobox(self.transfer_frame, value=school.names)
+        from_class.bind("<<ComboboxSelected>>", get_students_list)
+        from_class.grid(column=1, row=0, sticky=tk.W, padx=5, pady=5)
+        
+        label = ttk.Label(self.transfer_frame, text="Student Name: ")
+        label.grid(column=0, row=1, sticky=tk.W, padx=5, pady=5)
+        student_name = ttk.Combobox(self.transfer_frame, value=[" "])
+        student_name.grid(column=1, row=1, sticky=tk.W, padx=5, pady=5)
+        
+        label = ttk.Label(self.transfer_frame, text="To Class: ")
+        label.grid(column=0, row=2, sticky=tk.W, padx=5, pady=5)
+        to_class = ttk.Combobox(self.transfer_frame, value=school.names)
+        to_class.grid(column=1, row=2, sticky=tk.W, padx=5, pady=5)
+        
+        submit = ttk.Button(self.transfer_frame, text='submit', command=lambda:\
+                      [school.transfer_student(student_name.get(), school.classes[school.names.index(to_class.get())],\
+                                               school.classes[school.names.index(from_class.get())])])
+        submit.grid(column=2, row=3, sticky=tk.E, padx=5, pady=5)
+        
+        main_menu_button = ttk.Button(self.transfer_frame, text='Main Menu', command=lambda:\
+                      [self.main_win(school), self.transfer_frame.pack_forget()])
+        main_menu_button.grid(column=1, row=3, sticky=tk.E, padx=5, pady=5)
+    
     def take_attendance_win(self, school):
         self.take_attendance = tk.Frame(self)
         self.take_attendance.pack(fill="both", expand = True)
 
-        label = tk.Label(self.take_attendance, text="Class Name: ")
-        label.grid(column=0, row=0, sticky=tk.W, padx=5, pady=5)
-        class_name = tk.StringVar()
-        class_name.set("Choose")
-        name_class = tk.OptionMenu(self.take_attendance, class_name, *school.names if school.names else ["No classes"])
-        name_class.grid(column=1, row=0, sticky=tk.W, padx=5, pady=5)
+        def get_students_list(e):
+            var.config(value=school.get_class(class_list.get()))
 
-        label = tk.Label(self.take_attendance, text="Student Name: ")
+        label = ttk.Label(self.take_attendance, text="Class Name: ")
+        label.grid(column=0, row=0, sticky=tk.W, padx=5, pady=5)
+        class_list = ttk.Combobox(self.take_attendance, value=school.names)
+        class_list.bind("<<ComboboxSelected>>", get_students_list)
+        class_list.grid(column=1, row=0, sticky=tk.W, padx=5, pady=5)
+
+        label = ttk.Label(self.take_attendance, text="Student Name: ")
         label.grid(column=0, row=1, sticky=tk.W, padx=5, pady=5)
-        name_var = tk.StringVar()
-        name_var.set("Choose")
-        var = tk.OptionMenu(self.take_attendance, name_var, *school.classes[school.names.index(class_name.get())].students["name"]\
-                            if school.classes[school.names.index(class_name.get())].students["name"] else ["No students"])
+        var = ttk.Combobox(self.take_attendance, value=[" "])
         var.grid(column=1, row=1, sticky=tk.W, padx=5, pady=5)
 
-        submit = tk.Button(self.take_attendance, text='Submit', command=lambda:\
-                      [school.classes[school.names.index(name_var.get())].take_attendance(name_var.get())])
+        submit = ttk.Button(self.take_attendance, text='Submit', command=lambda:\
+                      [school.classes[school.names.index(var.get())].take_attendance(var.get())])
         submit.grid(column=3, row=5, sticky=tk.E, padx=5, pady=5)
         
-        main_menu_button = tk.Button(self.take_attendance, text='Main Menu', command=lambda:\
+        main_menu_button = ttk.Button(self.take_attendance, text='Main Menu', command=lambda:\
                       [self.main_win(school), self.take_attendance.pack_forget()])
         main_menu_button.grid(column=2, row=5, sticky=tk.E, padx=5, pady=5)
-        
-        
-        
-    
 
 if __name__ == "__main__":
     app = App()
